a8a60dc99eec1024eae064c5667b9e6a03ebe6dc
esteri1 <eadeyemi24@gmail.com>
2022-11-26 22:45:07 -0800

    state.py

--- models/state.py
+++ models/state.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""Defines the State class."""
+"""Module for State class."""
+
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Represent a state.
-
-    Attributes:
-        name (str): The name of the state.
-    """
-
+    """Class representing a State."""
     name = ""
