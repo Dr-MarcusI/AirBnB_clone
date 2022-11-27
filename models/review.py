521e8dd76ac88db373f04cc5e01e03f48eddbf51
esteri1 <eadeyemi24@gmail.com>
2022-11-26 22:43:42 -0800

    review.py

--- models/review.py
+++ models/review.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-"""Defines the Review class."""
+"""Module for Review class."""
+
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Represent a review.
-
-    Attributes:
-        place_id (str): The Place id.
-        user_id (str): The User id.
-        text (str): The text of the review.
-    """
-
+    """Class representing a Review."""
     place_id = ""
     user_id = ""
     text = ""
